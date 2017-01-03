906dc8943219ad24149afc38696d4c42816512cc
JMBarros <jmbarros@jmbarros13.local>
2017-01-02 22:26:16 -0200

    first

---
+++ controller/launcher.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+
+
+#######################################################################
+#defs
+def yum_install ( pkg ):
+   "function_docstring"
+   install = "yum install -y  "+ pkg
+   os.system(install)
+   return;
+
+def play_book ( pb ):
+   "function_docstring"
+   book = "ansible-playbook ./"+ pb
+   os.system(book)
+   return;
+
+def install_galaxy ( galaxy ):
+   "function_docstring"
+   ig = "ansible-galaxy install "+ galaxy
+   os.system(ig)
+   return;
+
+########################################################################
+
+yum_install( "epel-release" )
+yum_install( "ansible" )
+install_galaxy( "jmbarros.common")
+install_galaxy( "jmbarros.etcd")
+play_book ("install.yml")
+
+
